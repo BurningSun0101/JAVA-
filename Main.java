361ac6be410e2462d711b6320cebfc44c29c652b
BurningSun0101 <73688927+BurningSun0101@users.noreply.github.com>
2021-01-04 02:45:09 +0800

    Vtuber picture text

--- Main.java
+++ Main.java
@@ -1,53 +1,77 @@
-package com.company;
-import java.util.Scanner;
+package VtuberDemo;
+import javax.swing.*;
+import java.awt.event.*;
+
+class MyJFrame extends JFrame
+{
+    private JPanel contentPane;
+    String [] imgName = new String[]{"delta","魔乃","星接彗星","加萊耶冬雪","夜見"};
+    JButton btnPrev,btnNext;
+    JLabel lblName,lblImg,lblShow;
+    int n = 0;
+
+    MyJFrame()
+    {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100,100,230,310);
+        contentPane= new JPanel();
+        contentPane.setLayout(null);
+
+        lblImg= new JLabel();
+        lblImg.setIcon(new ImageIcon("F:\\text\\"+imgName[n]+".JPG"));
+        lblImg.setBounds(20,20,180,135);
+        contentPane.add (lblName);
+
+        lblName = new JLabel(imgName[n]);
+        lblName.setBounds(20,170,180,135);
+        contentPane.add(lblName);
+
+        String show = "第"+(n+1)+"張,共"+imgName.length+"張";
+        lblShow = new JLabel(show);
+        lblShow.setBounds(20,200,180,25);
+
+        btnPrev = new JButton("上一張");
+        btnPrev.setBounds(20,230,80,25);
+        contentPane.add(btnPrev);
+        btnPrev.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                n--;
+                if(n<0)
+                {
+                    n= imgName.length-1;
+                }
+                lblImg.setIcon(new ImageIcon("C:\\Users\\USER\\Desktop\\textpicture\\"+imgName[n]+".JPG"));
+                lblName.setText(imgName[n]);
+                lblShow.setText("第"+(n+1)+"張,共"+imgName.length+"張");
+
+            }
+        });
+
+        btnPrev = new JButton("下一張");
+        btnPrev.setBounds(20,230,80,25);
+        contentPane.add(btnPrev);
+        btnPrev.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                n++;
+                if(n>=imgName.length)
+                {
+                    n= 0;
+                }
+                lblImg.setIcon(new ImageIcon("C:\\Users\\USER\\Desktop\\textpicture\\"+imgName[n]+".JPG"));
+                lblName.setText(imgName[n]);
+                lblShow.setText("第"+(n+1)+"張,共"+imgName.length+"張");
+
+            }
+        });
+        setVisible(true);
+    }
+}
 
 public class Main {
 
-    public static void main(String[] args)
-    {
-        int number=0;//輸入餐點的號碼
-
-        int total=0;//總金額
-        Scanner cin = new Scanner(System.in);
-
-        String[] order = new String[3];//存取點的餐點
-
-        String[][] lunch;//品目陣列
-        lunch = new String[3][];
-        lunch[0] = new String[] {"1.凱薩沙拉","2.火腿三明治"};
-        lunch[1] = new String[] {"1.戰斧牛排","2.菲力牛排"};
-        lunch[2] = new String[] {"1.紅茶","2.奶茶","3.綠茶"};
-
-        int[][] money;//金額陣列
-        money = new int[3][];
-        money[0] = new int[] {20,25};
-        money[1] = new int[] {120,160};
-        money[2] = new int[] {20,25,20};
-
-        System.out.println("菜單");
-        for(int i=0;i<lunch.length;i++)
-        {
-            for (int k=0;k<lunch[i].length;k++)
-            {
-                System.out.print(lunch[i][k]+" "+money[i][k]+" ");
-            }
-            System.out.println(" ");
-        }
-
-        for(int i=0;i<lunch.length;i++)    //用以輸入餐點號碼
-        {
-            number = cin.nextInt();
-            number-=1;
-
-            total=total+money[i][number];
-
-            order[i]=lunch[i][number];
-
-        }
-
-        for (int i=0;i<order.length;i++)
-            System.out.print(order[i]+' ');
-        System.out.println(' ');
-        System.out.println("您本次餐點的金額"+" "+total);
+    public static void main(String[] args) {
+        MyJFrame f = new MyJFrame();
     }
 }
